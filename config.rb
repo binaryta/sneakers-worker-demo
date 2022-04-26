f6b1b7ed84db82ed323babbd7c9a498a429dca3c
binaryta <naritatakuya0000@gmail.com>
2022-04-27 02:44:27 +0900

    feat: update sneaker configuration

--- config.rb
+++ config.rb
@@ -2,6 +2,7 @@ require 'sneakers'
 
 Sneakers.configure(
   amqp:    ENV["RABBITMQ_URL"],
-  workers: ENV["SNEAKERS_WORKERS_SIZE"] || 1,
-  threads: ENV["SNEAKERS_THREADS_SIZE"] || 1,
+  workers: ENV["SNEAKERS_WORKERS_SIZE"] || 2,
+  threads: ENV["SNEAKERS_THREADS_SIZE"] || 2,
+  heartbeat: 0,
 )
